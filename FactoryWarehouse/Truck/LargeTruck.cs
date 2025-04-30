cc53bd563b3216c279552b83f1f14633d8ca3e42
m0tr1x <grigorysyzran@mail.ru>
2025-04-30 17:26:56 +0300

    made truck model and implementations

---
+++ FactoryWarehouse/Truck/LargeTruck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryWarehouse.Interfaces;
+
+namespace FactoryWarehouse.Truck
+{
+    /// <summary>
+    /// Implementation of base truck
+    /// </summary>
+    internal class LargeTruck : BaseTruck
+    {
+        public LargeTruck(IWarehouse warehouse) : base(warehouse, "Large Truck", 200)
+        {
+        }
+    }
+}
