e301aff4667a16be3a6b29eb3b48e7f5a7115c5f
m0tr1x <grigorysyzran@mail.ru>
2025-04-30 17:03:47 +0300

    made interfaces

---
+++ FactoryWarehouse/Interfaces/ITruck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryWarehouse.Interfaces
+{
+    internal interface ITruck
+    {
+        string Name { get; } // Truck name
+        int Capacity { get;  } // Capacity of truck
+        void StartDelivery(); // Method for start delivery
+        void StopDelivery(); // Method for start delivery
+    }
+}
