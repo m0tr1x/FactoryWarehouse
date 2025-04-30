550007dae7713d766d276fc09fadcc3f3c7073b2
m0tr1x <grigorysyzran@mail.ru>
2025-04-30 16:48:53 +0300

    made a project

---
+++ FactoryWarehouse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryWarehouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
