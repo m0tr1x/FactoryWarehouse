4f9802d7d080bf5f73ad1f7b16f0f75cb45c6ae9
m0tr1x <grigorysyzran@mail.ru>
2025-04-30 17:06:28 +0300

    made comms at IWarehouse.cs

--- FactoryWarehouse/Interfaces/IWarehouse.cs
+++ FactoryWarehouse/Interfaces/IWarehouse.cs
@@ -8,5 +8,11 @@ namespace FactoryWarehouse.Interfaces
 {
     internal interface IWarehouse
     {
+        bool AddProduct(string factoryName, IProduct product, int amount); // Method for addition to warehouse
+        int CurrentUsage {  get; } // Current usage of warehouse
+        int Capacity { get; } // Max capacity 
+
+        Dictionary<string, List<IProduct>> TakeProducts(int items); // Dictionary for taken products
+        event EventHandler CriticalCapacityReached; // Event for reaching 95% usage
     }
 }
